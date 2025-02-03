0f10db1b74937402f1a8f66d755f454dd67a0cd5
ACoder <sgy2788@163.com>
2025-02-03 12:00:58 +0800

    🐞 fix(sleep): catch errors

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -1,15 +1,25 @@
 #include <chrono>
+#include <stdexcept>
 #include <thread>
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != 2 || argc == 1) {
         std::cerr << "Usage: " << argv[0] << " <milliseconds>" << std::endl;
         return 1;
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(std::stoi(argv[1])));
+    try {
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::stoll(argv[1])));
+    } catch (std::invalid_argument &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    } catch (std::out_of_range &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    
 
     return 0;
 }
